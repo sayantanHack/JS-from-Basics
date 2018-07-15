5813da6343af3e5e4f435cb942f9195a6dc7676d
Sayantan <sayantandatta97@gmail.com>
2018-07-15 23:51:10 +0530

    adding variable

---
+++ 01 Basics/variable.js
@@ -0,0 +1,21 @@
+let name = 'Sayantan'; // let is used for variable .Const is used for contants
+// some times conts are also needed
+
+let num = '100'; //stil its a string
+let bonus ='20'
+
+let total= num + bonus; //string concatination
+console.log('concatination in string ->', total);
+console.log('Hello' , name) //js is very forgivable in semicolons
+
+let firstName = "Sayantan"
+let lastName = "Datta"
+
+console.log(firstName+' '+lastName); //simple concatination for adding two string
+
+/*
+wanna change firstname??
+just uncomment the bellow code... It`s` only possible for let not in conts
+*/
+// firtsName = "your name";
+// console.log(firstName);
