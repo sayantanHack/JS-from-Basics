4636e1a79e11d2eb65f7feddb5bcce22f3c68920
Sayantan <sayantandatta97@gmail.com>
2018-07-16 00:54:10 +0530

    null vs bool

---
+++ 01 Basics/nullandBool.js
@@ -0,0 +1,22 @@
+//temp is coming from website API
+
+var temp //This will print undefined which means there have no value, NULL .
+
+//var temp = 0
+//If you uncomment the just above code then you will get 0 which is a number(int or float) .
+//There have differece between ZERO & NULL .NULL is empty ,its an empty space .
+
+
+console.log("Current temerature is",temp);
+
+/* Boolean Operation - The operation is just deppend on TRUE or FALSE
+
+Let`s make a Grade system
+
+
+*/
+let marks = 7
+
+let myGrade = (marks <10)
+
+console.log(myGrade);
