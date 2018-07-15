0bddd2ee5f456b8fb39c42fbf104c2f5c35ca8ba
Sayantan <sayantandatta97@gmail.com>
2018-07-16 00:32:54 +0530

    farenheit to celsius conversion with basic algebra

---
+++ 01 Basics/celsius.js
@@ -0,0 +1,22 @@
+/* Before jump into the code area just warm up with
+basic algebra. uncomment it & use it.
+
+var score = 100;
+
+score = score + 30;
+
+const bonus = 50 ;
+var finalScore = (score + bonus)*1.8;
+
+var complex = (4 + 4)*5;
+
+console.log(finalScore);
+console.log(complex);
+*/
+let tempInFarenheit = 32;
+
+// formulla is C/5 = (F-32)*9
+
+let TempInCelsius = (tempInFarenheit - 32) *5/9
+
+console.log(TempInCelsius);
