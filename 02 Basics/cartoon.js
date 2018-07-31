ec46f5ec562e687795a9fc6445bd7118fa3381a5
sayantanHack <sayantandatta97@gmail.com>
2018-07-31 17:50:31 +0530

    Arrays & Template literals inJS

---
+++ 02 Basics/cartoon.js
@@ -0,0 +1,10 @@
+// Datatype  - Array
+
+
+const superHeros = ['Spiderman' , 'batman', 'superman']
+
+console.log(superHeros[1]);
+console.log('WE have',superHeros.length,'Super heros');
+//or you can also try this too..
+console.log(`We have ${superHeros.length} super Heros.`);
+console.log('Last item is - ',superHeros[superHeros.length - 1]);
