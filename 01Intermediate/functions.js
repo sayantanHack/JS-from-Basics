b67f93a1008620ed5f76572cf00a744eafc428c5
sayantanHack <sayantandatta97@gmail.com>
2018-07-31 22:00:31 +0530

    adding folder intermediate

---
+++ 01Intermediate/functions.js
@@ -0,0 +1,36 @@
+// ALL about functions
+
+// The bellow sayaHello is a fucntion decleration
+let sayHello = function(para) {
+  console.log(".......Getting from user");
+  console.log(`Hey ${para}!!!\n`);  // para means parameter
+}
+
+//calling functions
+sayHello('John');
+
+//  ---------- 2ND FUNCTION -------------
+
+let fullNameMaker = function(fName , lName) {
+    console.log('Welcome to our code ...');
+    console.log(`Happy to have you, ${fName} ${lName}!!!`);
+}
+
+fullNameMaker('John' , 'Cena');
+
+// ----------- 3RD FUNCTION in int or float  ---------------
+
+let sumVal = function(num1 , num2){
+  let added = num1 + num2;
+  console.log(`Hey The sum is ${added} .`);
+  // or
+  return added;
+}
+
+// if you wanna return this you should have to print(I mean console.log) the returned item.
+sumVal(3,5.9);
+console.log(sumVal(2,3.6));
+
+// you can store it into a variable too like...
+let result= sumVal(2,5);
+console.log(result);
