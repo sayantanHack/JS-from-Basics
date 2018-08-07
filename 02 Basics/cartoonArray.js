b6ce9870300e2c004680a8fc75ff7b67ea6dac1d
sayantanHack <sayantandatta97@gmail.com>
2018-08-07 19:30:19 +0530

    few added info in cartoonArray & v2 of it

--- 02 Basics/cartoonArray.js
+++ 02 Basics/cartoonArray.js
@@ -8,3 +8,27 @@ console.log('WE have',superHeros.length,'Super heros');
 //or you can also try this too..
 console.log(`We have ${superHeros.length} super Heros.`);
 console.log('Last item is - ',superHeros[superHeros.length - 1]);
+
+
+/*
+
+Lets take another array & use its fuctions like length ,capitalization etc.
+
+*/
+
+var names = [];
+
+names[0]='Ashik'
+names[1] = 'Rajeeb'
+names[2] = 'Rohan'
+names[3] = 'Neha'
+
+console.log('There have',names.length , 'values in it.');
+// Let`s print each name in uppercase
+for(var i =0; i < names.length; i++){
+  console.log('Hello', names[i].toUpperCase(),'!!');
+}
+
+// ext to find the index number of any particular item
+
+console.log(indexOf('Rohan')); // indexOf function helps to find the index number of the name entered.
