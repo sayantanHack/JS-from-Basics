2a0cbe285ca06b871b38316f9ee222045392afe8
sayantanHack <sayantandatta97@gmail.com>
2018-08-10 23:15:20 +0530

    added the Objects

--- 02 Intermediate/myObj.js
+++ 02 Intermediate/myObj.js
@@ -1,5 +1,12 @@
-// This is the
+// This is the Object orented part of JS .And the most vital in JS
+// A Class can be defined by simply adding {} after it .The objects are just inside of the Class
 
 let myVideo = {
+  title: 'JS from Basics',
+  length: 15,
+  Description: 'This is the description.'
+  price: 'FREE!!'
 
 }
+ console.log(myVideo);
+console.log(`Hey this is on ${myVideo.title} .This is a ${myVideo.length} minute video.This is ${myVideo.price}.`);
