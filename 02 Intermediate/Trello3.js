ec4a0e64e537731deeb6d037827031c93897fc8b
sayantanHack <sayantandatta97@gmail.com>
2018-08-20 11:59:18 +0530

    Method: Creating functions inside the class

---
+++ 02 Intermediate/Trello3.js
@@ -0,0 +1,13 @@
+//........... Methode creating ....
+// .......... Method is a special type of function inside in some class .....
+let myTodos ={
+  day: 'Monday',
+  meetings : 0,
+  meetDone: 0,
+
+  addMeeting : function(){
+    console.log("Hi! , I`m a method!!");
+  }
+}
+
+myTodos.addMeeting() // calling the method
