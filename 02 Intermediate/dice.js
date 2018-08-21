fd29e89278dec0b564aa35bc73a19a52fd3725e4
sayantanHack <sayantandatta97@gmail.com>
2018-08-22 00:54:04 +0530

    random number generate using Math function

--- 02 Intermediate/dice.js
+++ 02 Intermediate/dice.js
@@ -10,3 +10,9 @@ console.log(result.toFixed(2)); // runding up the number
 let floatResult = result.toFixed(2)
 
 console.log(Math.floor(floatResult));
+
+
+// ----------lets make some random number ------
+
+console.log('This is the original random num:',Math.random());
+console.log('Random number multiplied by 10: ',Math.random() * 10);
