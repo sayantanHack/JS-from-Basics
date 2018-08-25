d40e4c6e230b37448bbb70bef7807e7c413a6591
sayantanHack <sayantandatta97@gmail.com>
2018-08-25 23:30:25 +0530

    adding the function

--- 01 Advance/arrow.js
+++ 01 Advance/arrow.js
@@ -1,1 +1,9 @@
 // --  Not so hig advance things --- Advance things are based on the little little small topics
+
+// Let`screate  a simple function
+
+const sayHello = function(){
+  retrun "Hey there!! "+ name+"!!"
+}
+
+console.log(sayHello('Sayantan'));
