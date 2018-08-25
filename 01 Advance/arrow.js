6f95e27d7b43e15e1a2475916603cbdd5ed0bc72
sayantanHack <sayantandatta97@gmail.com>
2018-08-25 23:39:07 +0530

    adding arrow functions

--- 01 Advance/arrow.js
+++ 01 Advance/arrow.js
@@ -1,9 +1,19 @@
 // --  Not so hig advance things --- Advance things are based on the little little small topics
 
-// Let`screate  a simple function
+// Let`screate  a simple function .This is a normal function
 
 const sayHello = function(name){
   return "Hey there!! "+ name +"!!"
 }
 
 console.log(sayHello('Sayantan'));
+
+// -----  Here we will learn about the arrow functions -------
+// In arrow function we remove the function keyword & replacing with an ARROW(=>)
+
+const say =(name) => {
+  return "Hey there ,"+name+"!!"
+}
+console.log(say("Sayantan"));
+
+// reduce the code syntax & make the code is more flexible.
