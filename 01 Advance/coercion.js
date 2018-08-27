c0a1c54cd53670543338b4a3b209b2a6b9f8c14a
sayantanHack <sayantandatta97@gmail.com>
2018-08-28 01:56:28 +0530

    adding the smart features of JS

---
+++ 01 Advance/coercion.js
@@ -0,0 +1,4 @@
+//JS wanna be smart but dont use this feature
+
+console.log('5' - 6);// in this case it will substract
+console.log('4' + 5);  //but in this case JS cannot be so smart ;It only concatinate this
