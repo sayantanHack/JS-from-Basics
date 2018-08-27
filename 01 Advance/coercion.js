c927c1d2a2ec6f240a5ca5b37be958ec228e4c6c
sayantanHack <sayantandatta97@gmail.com>
2018-08-28 02:05:17 +0530

    added login type

--- 01 Advance/coercion.js
+++ 01 Advance/coercion.js
@@ -2,3 +2,21 @@
 
 console.log('5' - 6);// in this case it will substract
 console.log('4' + 5);  //but in this case JS cannot be so smart ;It only concatinate this
+// lets test what is it
+
+const giveType = typeof '5'
+
+console.log(giveType);
+
+const adder = true + 5 // true in jS mean its a literal 1 number
+console.log(adder);
+const loginDetailed = []
+// login for getting rom DB
+
+const loginID = logDetails[0]
+if(loginID !== undefigned){
+  console.log("allow user to login");
+}
+elase{
+  console.log('Auth  Failed!');
+}
