81276639ad839017f2066bec5bd306e9f702b739
sayantanHack <sayantandatta97@gmail.com>
2018-09-03 22:48:20 +0530

    Adding the Map object

---
+++ 01 Advance/mapsandforof.js
@@ -0,0 +1,20 @@
+var john = {
+  name:"I`m john",
+  age : 15,
+  isActive: true
+}
+
+var Mary = {
+  name:"I`m mary",
+  age : 25,
+  isActive: true
+}
+
+var Sam = {
+  name:"I`m sam",
+  age :23,
+  isActive: false
+}
+
+let users = new Map()
+console.log(typeof(users));
