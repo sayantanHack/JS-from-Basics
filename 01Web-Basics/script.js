4df62f46b00086a6ed33f307981a5596c6d97f21
sayantanHack <sayantandatta97@gmail.com>
2018-09-08 14:50:47 +0530

    adding few changes in looping

--- 01Web-Basics/script.js
+++ 01Web-Basics/script.js
@@ -1,1 +1,16 @@
-alert("file is attached")
+// the goal is how to change the contet of the paragraphs
+//const myElem =document.querySelectorAll('.clsone')
+
+const myParaElem = document.querySelector('p')
+console.log(myParaElem);
+
+myParaElem.textContent = "I`m changing for JS"
+// now see in your browser the 1st paragraph get changed
+/* If you wanna do the same changes in other any tags u can. In case of this there have another
+    <p> tags by using looping
+*/
+const myPElem = document.querySelectorAll('p')
+
+myPElem.forEach(function(p){
+  p.textContent = 'I am changed into looping ...'
+})
