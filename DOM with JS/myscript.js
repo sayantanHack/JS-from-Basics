24c3e579651c8673b13706e7e2a3c35f9222f690
sayantanHack <sayantandatta97@gmail.com>
2018-09-12 23:20:18 +0530

    comment uncomment

--- DOM with JS/myscript.js
+++ DOM with JS/myscript.js
@@ -1,7 +1,7 @@
 // to see what is inside the title type bellow command
 //  To see the changes you just need to check in inspect mode console of ur browser
-// do uncomment & comment to use codes 
-/*
+// do uncomment & comment to use codes
+
 console.log(document.title);
 
 // or to change anything in any tag
@@ -11,7 +11,7 @@ document.title = "Now I`m changed"
 console.log(document.getElementById('idone'));
 console.log(document.getElementsByClassName('cls'));
 // There have a good option to select query
-*/
+
 const myElement = document.querySelectorAll("p")
 // selectiong id & clases is just like CSS
 const myElementId = document.querySelectorAll('#idone')
