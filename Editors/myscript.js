a192d47acde4e6387c294bfeff488a499b516cd2
sayantanHack <sayantandatta97@gmail.com>
2018-09-12 23:17:59 +0530

    Queryselection

--- Editors/myscript.js
+++ Editors/myscript.js
@@ -1,4 +1,7 @@
 // to see what is inside the title type bellow command
+//  To see the changes you just need to check in inspect mode console of ur browser
+// do uncomment & comment to use codes 
+/*
 console.log(document.title);
 
 // or to change anything in any tag
@@ -7,3 +10,11 @@ document.title = "Now I`m changed"
 // now let`s  use tag selecting by id or class
 console.log(document.getElementById('idone'));
 console.log(document.getElementsByClassName('cls'));
+// There have a good option to select query
+*/
+const myElement = document.querySelectorAll("p")
+// selectiong id & clases is just like CSS
+const myElementId = document.querySelectorAll('#idone')
+const myElementCl = document.querySelectorAll(".cls")
+
+console.log(myElement , myElementCl, myElementId);
