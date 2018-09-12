a3966f4162ae0ea82bb02fc072dd2550c10df9d0
sayantanHack <sayantandatta97@gmail.com>
2018-09-12 23:07:02 +0530

    getElements

--- Editors/myscript.js
+++ Editors/myscript.js
@@ -1,5 +1,9 @@
 // to see what is inside the title type bellow command
 console.log(document.title);
 
-// or to change anything in any tag 
+// or to change anything in any tag
 document.title = "Now I`m changed"
+
+// now let`s  use tag selecting by id or class
+console.log(document.getElementById('idone'));
+console.log(document.getElementsByClassName('cls'));
