7e013944b301edb0029fcc1a672b13ee6c6d6720
sayantanHack <sayantandatta97@gmail.com>
2018-09-18 14:42:06 +0530

    adding

--- 01 Basics/test.js
+++ 01 Basics/test.js
@@ -5,9 +5,17 @@ function add(x,y){
 console.log(add(2,3));  // 5
 console.log(typeof add(2,3));  // it is Integer
 
-console.log(add(2.0,3.0));   // 5.0
-console.log(typeof add(2.0,3.0));  // it is float
+console.log(add(0.2,0.3));   // 5.0
+console.log(typeof add(0.2,0.3));  // it is float
 // but JS does not diferenciate the numbers into int float or double
 
 console.log(add('2','3'));  // 23 it will not be added
 console.log(typeof add('2','3'));  // cause its a string
+
+// let`s check the & make it more affective
+
+function addTwo(x,y){
+  if((typeof x && typeof y) !== 'number'){
+    throw new Error("parameter must be a number")
+  }
+}
