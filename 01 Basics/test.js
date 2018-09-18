868ed675cbbd04d584ca19e37acc4b9f11fcc075
sayantanHack <sayantandatta97@gmail.com>
2018-09-18 12:06:38 +0530

    numbers & strings

--- 01 Basics/test.js
+++ 01 Basics/test.js
@@ -3,6 +3,11 @@ function add(x,y){
 }
 
 console.log(add(2,3));  // 5
-console.log(add(2,3));   // 5
+console.log(typeof add(2,3));  // it is Integer
+
+console.log(add(2.0,3.0));   // 5.0
+console.log(typeof add(2.0,3.0));  // it is float
+// but JS does not diferenciate the numbers into int float or double
 
 console.log(add('2','3'));  // 23 it will not be added
+console.log(typeof add('2','3'));  // cause its a string
