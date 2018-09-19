ab9cdd06d08d7e2447e000045ce11f83d56447c1
sayantanHack <sayantandatta97@gmail.com>
2018-09-19 17:09:56 +0530

    added the OOP file

---
+++ OOP concept/oop1.js
@@ -0,0 +1,11 @@
+// This is the object oriented Javascript here will be class constructor methods & objects
+
+'use strict'; // strict code
+
+class  User{
+  constructor(username, email, password){
+    this.userName = username;
+    this.email = email;
+    this.password = password;
+  }
+}
