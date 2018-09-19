9d12e8805f677106815e921ce7578e4f73e0cfde
sayantanHack <sayantandatta97@gmail.com>
2018-09-19 20:23:13 +0530

    class constructor & new method is called

--- OOP concept/oop1.js
+++ OOP concept/oop1.js
@@ -1,11 +1,20 @@
 // This is the object oriented Javascript here will be class constructor methods & objects
 
 'use strict'; // strict code
-
+// this is the typical class
 class  User{
-  constructor(username, email, password){
+  constructor(username, email, password){ //its constructor
     this.userName = username;
     this.email = email;
     this.password = password;
   }
+
+  registered(){    // its a method
+    console.log(this.userName+" is now registered.");
+  }
 }
+
+// Now bob is calling the User method using the his name mail & pass
+let bob = new User('Bob','bob@email.com','123456');
+// bob is calling register method
+bob.register();
