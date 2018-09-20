0690d608a84776c8788588fcbd7b3a2e0a9c496e
sayantanHack <sayantandatta97@gmail.com>
2018-09-20 19:47:23 +0530

    added extended class .which is using the superclass.

---
+++ OOP concept/extended_Class.js
@@ -0,0 +1,32 @@
+'use strict'; // strict code
+// this is the typical class
+class  User{
+  constructor(username, email, password){ //its constructor
+    this.userName = username;
+    this.email = email;
+    this.password = password;
+  }
+// lets create a static method
+  static countUser(){
+      console.log("There have 50 users");
+  }
+
+  registered(){    // its a method
+    console.log(this.userName+" is now registered.");
+  }
+}
+
+class Member extends User{
+  constructor(username, email, password, numberPackage){
+    super(username, email,password);
+    this.package = numberPackage;
+  }
+
+  getPackage(){
+    console.log(this.userName+'is subscribed to the '+this.package + 'package');
+
+  }
+}
+
+let Mike = new Member("Mike",'mike@email.com','1234', 'Standard');
+Mike.getPackage();
