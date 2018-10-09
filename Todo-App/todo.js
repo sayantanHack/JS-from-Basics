c434f2d352f5498a8738b92c7b00fb7c9a16f1a6
sayantanHack <sayantandatta97@gmail.com>
2018-10-09 20:02:26 +0530

    changing in JS

--- Todo-App/todo.js
+++ Todo-App/todo.js
@@ -1,6 +1,21 @@
-var val = 5;
 
-while (val >0) {
-    console.log(val);
-    val--; // or val = val-1
+var ul = document.getElementById('list');
+var li;
+
+var addButton = document.getElementById('add');
+addButton.addEventListner('click',addItem);
+
+var removeButton = document.getElementById('remove');
+removeButton.addEventListner('click',removeButton);
+
+function addItem() {
+  var input = document.getElementById('input')
+  var item = input.value;
+  ul = document.getElementsById('list');
+  var textnode = document.createTextNode(item)
+
+if(item === ''){
+  return false;
+  }
+
 }
