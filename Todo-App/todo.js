579f6dccf546392b25aee4877e21a6b79e373ba9
sayantanHack <sayantandatta97@gmail.com>
2018-10-09 22:02:18 +0530

    added JS

--- Todo-App/todo.js
+++ Todo-App/todo.js
@@ -29,7 +29,12 @@ else{
   label.setAttribute('for','item') //optional
 
   // adding these to page
-  
+  ul.appendChild(label);
+  li.appendChild(checkbox);
+  label.appendChild(textnode);
+  li.appendChild(label);
+  ul.insertBefore(li, ul.childNodes[0]);
+  li.className = 'visual';
 
   }
 
