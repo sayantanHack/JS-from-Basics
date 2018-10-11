67c812385b4065853b39d8faee4ae9566944a9a2
sayantanHack <sayantandatta97@gmail.com>
2018-10-11 21:55:05 +0530

    added self defined function in User class

--- 02Advance/newClass.js
+++ 02Advance/newClass.js
@@ -5,6 +5,18 @@ class User {
     this.last = lastname
     this.credit = credit
   }
+  // new method user defined
+// inside of the Class we no need to write the function keyword before the function name
+
+    nofullName(){
+      let fullName = `${this.first} ${this.last} is my full name.`
+      return fullName;
+    }
+
+
+
 }
 const john = new User('John',"Datta",36)
-const sammy = new.User("Sammy","Paul",23)
+const sammy = new User("Sammy","Paul",23)
+
+console.log(sammy.nofullName());
