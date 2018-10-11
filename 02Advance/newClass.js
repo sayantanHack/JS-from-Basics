9ef0f6ff7baca735faff6b16f0b7876b5a0d4958
sayantanHack <sayantandatta97@gmail.com>
2018-10-11 21:41:05 +0530

    added objects

--- 02Advance/newClass.js
+++ 02Advance/newClass.js
@@ -1,7 +1,10 @@
 // 1st letter of any class should be Uppercase (not compulsarry)
 class User {
-  constructor() {
-
+  constructor(firstname,lastname, credit) {
+    this.first = firstname
+    this.last = lastname
+    this.credit = credit
   }
 }
-const john = new User()
+const john = new User('John',"Datta",36)
+const sammy = new.User("Sammy","Paul",23)
