76c67aa5cc90f924a5c8583fc36f88d6ebcf9d21
sayantanHack <sayantandatta97@gmail.com>
2018-10-11 23:22:41 +0530

    added split method for split elements into array

--- 02Advance/newClass.js
+++ 02Advance/newClass.js
@@ -12,11 +12,18 @@ class User {
       let fullName = `${this.first} ${this.last} is my full name.`
       return fullName;
     }
-
+    editname(newName){
+      const myName = newName.split(' ');
+      this.first = myName[0]
+      this.last = myName[1]
+    }
 
 
 }
 const john = new User('John',"Datta",36)
 const sammy = new User("Sammy","Paul",23)
 
+john.editname('Johny Man')
 console.log(sammy.fullName());
+
+console.log(john.fullName());
