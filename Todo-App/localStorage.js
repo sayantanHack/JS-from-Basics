f9f23d9dd298c558f96b9093824da7d02d4fccc5
sayantanHack <sayantandatta97@gmail.com>
2018-10-14 23:55:27 +0530

    deleteing storage

--- Todo-App/localStorage.js
+++ Todo-App/localStorage.js
@@ -4,3 +4,7 @@ console.log(myHero);
 
 localStorage.setItem('todo',"tutorial");
 console.log(localStorage.getItem('todo'));
+
+// Cleaning local storage is also easy
+localStorage.clear();
+// It will clear entire storage
